04a7f3c90edbff014ff25b125565c1ceeb6e6f08
Sergey Babkin <sergeybabkin1@gmail.com>
2023-06-07 12:17:31 +0300

    feat: Data-base settings were added.

---
+++ src/main/java/org/example/config/RedDataBaseDialectResolver.java
@@ -0,0 +1,30 @@
+package org.example.config;
+
+import org.springframework.data.jdbc.repository.config.DialectResolver;
+import org.springframework.data.relational.core.dialect.Dialect;
+import org.springframework.jdbc.core.ConnectionCallback;
+import org.springframework.jdbc.core.JdbcOperations;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import java.util.Locale;
+import java.util.Optional;
+
+public class RedDataBaseDialectResolver implements DialectResolver.JdbcDialectProvider {
+
+    @Override
+    public Optional<Dialect> getDialect(JdbcOperations operations) {
+        return Optional.ofNullable(
+                operations.execute((ConnectionCallback<Dialect>) RedDataBaseDialectResolver::getDialect));
+    }
+
+    private static Dialect getDialect(Connection connection) throws SQLException  {
+        DatabaseMetaData metaData = connection.getMetaData();
+        String name = metaData.getDatabaseProductName().toLowerCase(Locale.ROOT);
+        if (name.contains("reddatabase")) {
+            return RedDataBaseDialect.INSTANCE;
+        }
+        return null;
+    }
+}
